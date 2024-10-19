8ad26bf9ad8badf00a0c68a51b6fe639079e01c9
Minssuy99 <rlaalstjd6302@naver.com>
2024-10-19 18:10:15 +0900

    2024-10-19 [Practice] Update > 8_3.cpp

---
+++ Practice/C++/8_Chapter/8_3_생성자.cpp
@@ -0,0 +1,22 @@
+/* 생성자 */
+
+#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+  private:
+    int m_numerator;
+    int m_denominator;
+
+  public:
+    void print()
+    {
+        cout << m_numerator << " / " << m_denominator << endl;
+    }
+};
+
+int main()
+{
+}

256aafaf5b7d755f852ad5d795141a12bc8418af
Minssuy99 <rlaalstjd6302@naver.com>
2024-10-19 18:36:27 +0900

    2024-10-19 [Practice] Update > 8_3.cpp

--- Practice/C++/8_Chapter/8_3_생성자.cpp
+++ Practice/C++/8_Chapter/8_3_생성자.cpp
@@ -11,6 +11,19 @@ class Fraction
     int m_denominator;
 
   public:
+    Fraction() // Constructors
+    {
+        m_numerator = 0;
+        m_denominator = 1;
+    }
+
+    // = 1 이라고 하면, 매개변수를 입력하지 않았을 때 기본값이 1이 되도록 설정한다.
+    Fraction(const int &num_in = 1, const int &den_in = 1) // 생성자도 오버로딩이 된다.
+    {
+        m_numerator = num_in;
+        m_denominator = den_in;
+    }
+
     void print()
     {
         cout << m_numerator << " / " << m_denominator << endl;
@@ -19,4 +32,6 @@ class Fraction
 
 int main()
 {
+    Fraction frac();
+    frac().print();
 }
